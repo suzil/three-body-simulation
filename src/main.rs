8d0ce10706d9533a20a3202aa686490bda3d0e44
Susannah Klaneček <suzil@protonmail.ch>
2021-02-14 14:30:24 -0600

    Add a start button

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy::render::pass::ClearColor;
+use bevy_egui::{egui, EguiContext, EguiPlugin};
 use ndarray::prelude::*;
 
 struct Star;
@@ -13,9 +14,15 @@ struct Materials {
     star_material: Handle<ColorMaterial>,
 }
 
+struct UiState {
+    started: bool,
+}
+
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
+        .add_plugin(EguiPlugin)
+        .add_system(ui.system())
         .add_resource(WindowDescriptor {
             title: "Three Celestial Bodies".to_string(),
             width: 1000.0,
@@ -24,10 +31,6 @@ fn main() {
         })
         .add_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .add_startup_system(setup.system())
-        .add_startup_stage(
-            "simulation_setup",
-            SystemStage::single(spawn_stars.system()),
-        )
         .add_system(star_movement.system())
         .add_system(position_translation.system())
         .run();
@@ -43,44 +46,57 @@ fn setup(
         .spawn(Camera2dBundle::default())
         .insert_resource(Materials {
             star_material: materials.add(texture_handle.into()),
-        });
+        })
+        .insert_resource(UiState { started: false });
 }
 
-fn spawn_stars(commands: &mut Commands, materials: Res<Materials>) {
-    commands
-        .spawn(SpriteBundle {
-            material: materials.star_material.clone(),
-            transform: Transform::from_scale(Vec3::splat(0.3)),
-            ..Default::default()
-        })
-        .with(Star)
-        .with(State {
-            position: array![200.0, 0.0],
-            momentum: array![0.0, 20.0],
-            mass: 1.0,
-        })
-        .spawn(SpriteBundle {
-            material: materials.star_material.clone(),
-            transform: Transform::from_scale(Vec3::splat(0.3)),
-            ..Default::default()
-        })
-        .with(Star)
-        .with(State {
-            position: array![0.0, 0.0],
-            momentum: array![5.0, 0.0],
-            mass: 10.0,
-        })
-        .spawn(SpriteBundle {
-            material: materials.star_material.clone(),
-            transform: Transform::from_scale(Vec3::splat(0.3)),
-            ..Default::default()
-        })
-        .with(Star)
-        .with(State {
-            position: array![-200.0, 0.0],
-            momentum: array![0.0, -20.0],
-            mass: 1.0,
-        });
+fn ui(
+    commands: &mut Commands,
+    mut ui_state: ResMut<UiState>,
+    materials: Res<Materials>,
+    mut egui_context: ResMut<EguiContext>,
+) {
+    let ctx = &mut egui_context.ctx;
+    egui::Window::new("Control Panel").show(ctx, |ui| {
+        ui.set_enabled(!ui_state.started);
+        if ui.button("Start").clicked() && !ui_state.started {
+            commands
+                .spawn(SpriteBundle {
+                    material: materials.star_material.clone(),
+                    transform: Transform::from_scale(Vec3::splat(0.3)),
+                    ..Default::default()
+                })
+                .with(Star)
+                .with(State {
+                    position: array![200.0, 0.0],
+                    momentum: array![0.0, 20.0],
+                    mass: 1.0,
+                })
+                .spawn(SpriteBundle {
+                    material: materials.star_material.clone(),
+                    transform: Transform::from_scale(Vec3::splat(0.3)),
+                    ..Default::default()
+                })
+                .with(Star)
+                .with(State {
+                    position: array![0.0, 0.0],
+                    momentum: array![5.0, 0.0],
+                    mass: 10.0,
+                })
+                .spawn(SpriteBundle {
+                    material: materials.star_material.clone(),
+                    transform: Transform::from_scale(Vec3::splat(0.3)),
+                    ..Default::default()
+                })
+                .with(Star)
+                .with(State {
+                    position: array![-200.0, 0.0],
+                    momentum: array![0.0, -20.0],
+                    mass: 1.0,
+                });
+            ui_state.started = true;
+        };
+    });
 }
 
 fn position_translation(mut query: Query<(&State, &mut Transform)>) {
@@ -101,6 +117,9 @@ fn star_movement(mut star_states: Query<&mut State, With<Star>>) {
     let mut all_states = vec![];
     for state in star_states.iter_mut() {
         all_states.push(state);
+    }
+    if all_states.len() != 3 {
+        return;
     }
 
     const G: f32 = 10000.0;
